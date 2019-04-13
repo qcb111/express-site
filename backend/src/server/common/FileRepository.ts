8d2dbc8efc84b66b9f215d79357f7cf7127a9c97
qcb <qcbjohn@gmail.com>
2019-04-13 15:57:52 +0800

    create FileRepository class to use file as db

---
+++ backend/src/server/common/FileRepository.ts
@@ -0,0 +1,59 @@
+import fs = require('fs');
+
+const repositoryFolder = './data';
+
+class FileRepository<T>{
+    private collection: Array<T>;
+    private filename: string;
+
+    public constructor(repoName: string) {
+        if(!fs.existsSync(repositoryFolder)){
+            fs.mkdirSync(repositoryFolder);
+        }
+        this.filename = `${repositoryFolder}/${repoName}.json`
+        if (fs.existsSync(this.filename)) {
+            let fileContent = fs.readFileSync(this.filename, 'utf8');
+            this.collection = JSON.parse(fileContent);
+        } else {
+            this.collection = [];
+        }
+
+    };
+
+    public insertOne(document: T): void{
+        this.collection.push(document);
+        this.save();
+    };
+
+    public insert(documents: Array<T>): void{
+        this.collection = this.collection.concat(documents);
+        this.save();
+    }
+
+    public get(selector: (doc: T)=>Boolean): Array<T>{
+        return this.collection.filter(selector);
+    };
+
+    public delete(selector: (doc: T)=>Boolean): number{
+        let originCount = this.collection.length;
+        this.collection = this.collection.filter(doc=>!selector(doc));
+        let currentCount = this.collection.length;
+        this.save();
+        return originCount - currentCount;
+    };
+
+    public update(selector: (doc: T)=>Boolean, newDoc: T): number {
+        let changedCount = this.delete(selector);
+        for(let i=0;i<changedCount;i++){
+            this.collection.push(newDoc);
+        }
+        this.save();
+        return changedCount;
+    }
+
+    private save(): void{
+        fs.writeFileSync(this.filename, JSON.stringify(this.collection));
+    }
+}
+
+export default FileRepository;
--- backend/src/server/controllers/todolistController.ts
+++ backend/src/server/controllers/todolistController.ts
@@ -1,53 +1,43 @@
 import express = require("express");
-import fs = require('fs'); 
-import {TodoItem} from '../models/todolist';
+import fs = require('fs');
+import { TodoItem } from '../models/todolist';
+import FileRepository from '../common/FileRepository';
 
-const file = `./data/todolist.json`;
+const collectionName = `todolist`;
 class TodolistController {
     public path = '/todolist';
     public router = express.Router();
-    
-    private list: TodoItem[] = [
-        {
-            content: 'Learn German!',
-            isFinished: false
-        }
-    ];
-   
+
+    private collection: FileRepository<TodoItem>;
+
+
     constructor() {
-      this.intializeRoutes();
-      if(fs.existsSync(file)){
-          let fileContent = fs.readFileSync(file, 'utf8');
-          this.list = JSON.parse(fileContent);
-      } else {
-          this.list = [];
-      }
+        this.intializeRoutes();
+        this.collection = new FileRepository(collectionName);
     }
-   
+
     public intializeRoutes() {
-      this.router.get(this.path, this.getList);
-      this.router.put(this.path, this.addItem);
-      this.router.delete(`${this.path}/:content`, this.removeItem);
+        this.router.get(this.path, this.getList);
+        this.router.put(this.path, this.addItem);
+        this.router.delete(`${this.path}/:content`, this.removeItem);
     }
-   
+
     getList = (request: express.Request, response: express.Response) => {
-      response.status(200).json(this.list);
+        let list = this.collection.get(()=>true);
+        response.status(200).json(list);
     };
-   
+
     addItem = (request: express.Request, response: express.Response) => {
-      const item: TodoItem = request.body;
-      this.list.push(item);
-      fs.writeFileSync(file, JSON.stringify(this.list));
-      response.status(201).json(item);
+        const item: TodoItem = request.body;
+        this.collection.insertOne(item);
+        response.status(201).json(item);
     };
 
     removeItem = (req: express.Request, res: express.Response) => {
         const con = req.params.content;
-        this.list = this.list.filter((item)=>item.content!=con);
-        fs.writeFileSync(file, JSON.stringify(this.list));
-        res.status(200).json(this.list);
+        let count = this.collection.delete((item) => item.content === con);
+        res.status(200).json({deleted: count});
     }
-  }
-   
-  export default TodolistController;
-  
+}
+
+export default TodolistController;
